374652768e5aaf1a3774eb77b6e1033d00f52fae
Hxiang <xiang792178903@sina.com>
2016-09-24 23:33:55 +0800

    fdfdfdvfdvvded

---
+++ ZhiMaBiaoPan/ZhiMaXinYongDemo/app/src/main/java/com/jackson/zhimaxinyongdemo/AddActivity.java
@@ -0,0 +1,17 @@
+package com.jackson.zhimaxinyongdemo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2016/9/24.
+ */
+public class AddActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+}
